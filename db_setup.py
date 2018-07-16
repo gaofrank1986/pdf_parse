7837b09d00172283e270ff7333d0e0cc38ef3394
home <home@homedeMacBook-Pro.local>
2018-07-16 21:03:09 +0800

    make small changes to db creation

--- db_setup.py
+++ db_setup.py
@@ -17,7 +17,6 @@ class Info(Base):
         self.fm_id = str(stock_id)+'_'+str(date)
         self.stock_id = stock_id
         self.stock_name = "unkown"
-        self.unit = 0.0
         self.date = date
         self.date_postfix = 'A'
 
@@ -46,43 +45,43 @@ class Concise_Table(Base):
         self.fm_id = str(stock_id)+'_'+str(date)
         self.stock_id = stock_id
         self.stock_name = "unkown"
-        self.unit = 0.0
         self.date = date
         self.date_postfix = 'A'
+        self.unit = ''
 
         self.cash_and_eq = 0.0
         self.note_rcv    = 0.0
         self.acc_rcv     = 0.0
-        pre_paid     = 0.0
-        other_rcv     = 0.0
+        self.pre_paid     = 0.0
+        self.other_rcv     = 0.0
         self.inventory   = 0.0
         self.total_liquid_asset = 0.0
         self.total_asset = 0.0
 
-        note_pyb    = 0.0
-        acc_pyb    = 0.0
-        pre_rcv    = 0.0
-        other_pyb    = 0.0
+        self.note_pyb    = 0.0
+        self.acc_pyb    = 0.0
+        self.pre_rcv    = 0.0
+        self.other_pyb    = 0.0
         self.fixed_asset = 0.0
         self.on_building = 0.0
         self.total_liquid_debt  = 0.0
         self.total_debt         = 0.0
         self.equity      = 0.0
 
-        total_revenue = 0.0
+        self.total_revenue = 0.0
         self.revenue = 0.0
         self.op_cost = 0.0
         self.sale_expense = 0.0
         self.admin_expense = 0.0
         self.finance_expense = 0.0
-        invst_gain = 0.0
+        self.invst_gain = 0.0
         self.op_profit = 0.0
-        total_profit = 0.0
-        incm_tax = 0.0
+        self.total_profit = 0.0
+        self.incm_tax = 0.0
         self.net_profit = 0.0
 
-        cash_inflow_from_op = 0.0
+        self.cash_inflow_from_op = 0.0
         self.net_cash_from_op = 0.0
 
-        smr_deducted_net_profit = 0.0
-        smr_avg_roe = 0.0
+        self.smr_deducted_net_profit = 0.0
+        self.smr_avg_roe = 0.0
