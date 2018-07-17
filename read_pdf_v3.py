ff4fcfa40d19f48e02b2bd1470398f49e770cce2
home <home@homedeMacBook-Pro.local>
2018-07-17 12:31:20 +0800

    new read_pdf

--- read_pdf_v3.py
+++ read_pdf_v3.py
@@ -4,39 +4,45 @@ from sqlalchemy import exists
 from pprint import pprint
 
 a = FmUtils()
-#  for year in range(2011,2018):
-    #  path = "./kldt_"+str(year)+"A0.pdf"
+for ii in range(2011,2018):
+    name = '康力电梯'
+    b = a.load_from_json("./"+name+"/info.json")
+    key = name+str(ii)+'.pdf'
+    v = b[key]
+    c = v['table_pages']
+    path = './' + name + '/' + key
+    pprint(path)
+    try:
+        pages = str(c[0])+'-'+str(c[1])
+        d = a.read_and_clean(path,pages,mode = 1)
+        pprint(d)
 
-#  r,e = a.get_ratios(d)
-b = a.load_from_json("./伟星新材/info.json")
-b = dict(b)
-key = '伟星新材2014.pdf'
-v = b[key]
-c = v['table_pages']
-path = './伟星新材/'+key
-pages = str(c[0])+'-'+str(c[1])
-d = a.read_and_clean(path,pages,mode = 1)
-pprint(d)
+        keys = [str(v['stock_id'])+'_'+str(v['year']), str(v['stock_id'])+'_'+str(int(v['year'])-1)]
+        name = a.load_from_json("./json/table_trans.json")
+        for a_key in keys:
+            (res,) =db_info.query(exists().where(Concise_Table.fm_id==a_key))
+            if not(res[0]):
+                db_info.add(Concise_Table(stock_id = v['stock_id'],date = a_key[-4:]))
+                db_info.commit()
 
-keys = [str(v['stock_id'])+'_'+str(v['year']), str(v['stock_id'])+'_'+str(int(v['year'])-1)]
-name = a.load_from_json("./table_trans.json")
-for a_key in keys:
-    (res,) =db_info.query(exists().where(Concise_Table.fm_id==a_key))
-    if not(res[0]):
-        db_info.add(Concise_Table(stock_id = v['stock_id'],date = a_key[-4:]))
+        for a_key in keys:
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).\
+                    update({"stock_name": v['stock_name']})
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).update({"unit": v['unit']})
         db_info.commit()
-    #  是否考虑，整体
-for a_key in keys:
-    db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).update({"stock_name": v['stock_name']})
-db_info.commit()
-for i in d.keys():
-    tmp = name.get(i,-99)
-    if tmp!="0" and tmp!=-99:
-        db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[i]: d[i][0]})
-        db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[i]: d[i][1]})
-    elif tmp==-99:
-        print(i)
+        for i in d.keys():
+            tmp = name.get(i,-99)
+            if tmp!="0" and tmp!=-99:
+                db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[i]: d[i][0]})
+                db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[i]: d[i][1]})
+            elif tmp==-99:
+                print(i)
 
-db_info.commit()
+        db_info.commit()
+    except:
+        #write in log
+        with open("./log.txt","w") as f:
+            f.write("Error when extracting tables from " + key +"\n")
+
 
 
