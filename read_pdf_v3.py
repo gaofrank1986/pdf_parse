c5ddb2021924f4cc2b9de378c07e8f63c899eaac
home <home@homedeMacBook-Pro.local>
2018-07-23 12:29:12 +0800

    some updates

--- read_pdf_v3.py
+++ read_pdf_v3.py
@@ -2,47 +2,90 @@ from fm_utils import FmUtils
 from db_setup import Info,db_info,Concise_Table
 from sqlalchemy import exists
 from pprint import pprint
+import os
 
 a = FmUtils()
 for ii in range(2011,2018):
-    name = '康力电梯'
-    b = a.load_from_json("./"+name+"/info.json")
-    key = name+str(ii)+'.pdf'
+    stock_name = '万华化学'
+    mode = 1
+    # -------------init -------------------
+    b = a.load_from_json("./"+stock_name+"/info.json")
+    key = stock_name+str(ii)+'.pdf'
     v = b[key]
     c = v['table_pages']
-    path = './' + name + '/' + key
-    pprint(path)
-    try:
-        pages = str(c[0])+'-'+str(c[1])
-        d = a.read_and_clean(path,pages,mode = 1)
-        pprint(d)
+    if v['mode']!= 99:
+        mode = v['mode']
+    dirc = './' + stock_name + '/'
+    pprint(dirc+key)
+    #  try:
+    pages = str(c[0])+'-'+str(c[1])
+    d = a.read_and_clean(dirc+key,pages,mode = mode)
+    pprint(d)
 
-        keys = [str(v['stock_id'])+'_'+str(v['year']), str(v['stock_id'])+'_'+str(int(v['year'])-1)]
-        name = a.load_from_json("./json/table_trans.json")
-        for a_key in keys:
-            (res,) =db_info.query(exists().where(Concise_Table.fm_id==a_key))
-            if not(res[0]):
-                db_info.add(Concise_Table(stock_id = v['stock_id'],date = a_key[-4:]))
-                db_info.commit()
+    # ------------ init json ------------
+    name = a.load_from_json("./json/table_trans.json")
+    json_loc = dirc + stock_name + str(int(v['year'])-1)+'.json'
+    if os.path.exists(json_loc):
+        set_last = a.load_from_json(json_loc)
+        print("found")
+    else:
+        set_last = a.load_from_json("./json/reports_template.json")
+    set_this = a.load_from_json("./json/reports_template.json")
 
-        for a_key in keys:
-            db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).\
-                    update({"stock_name": v['stock_name']})
-            db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).update({"unit": v['unit']})
-        db_info.commit()
-        for i in d.keys():
-            tmp = name.get(i,-99)
-            if tmp!="0" and tmp!=-99:
-                db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[i]: d[i][0]})
-                db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[i]: d[i][1]})
-            elif tmp==-99:
-                print(i)
+    # ---------operate db----------------
+    db_keys = [str(v['stock_id'])+'_'+str(v['year']), str(v['stock_id'])+'_'+str(int(v['year'])-1)]
+    for a_key in db_keys:
+        (res,) =db_info.query(exists().where(Concise_Table.fm_id==a_key))
+        if not(res[0]):
+            db_info.add(Concise_Table(stock_id = v['stock_id'],date = a_key[-4:]))
+            db_info.commit()
 
-        db_info.commit()
-    except:
-        #write in log
-        with open("./log.txt","w") as f:
-            f.write("Error when extracting tables from " + key +"\n")
+    for a_key in db_keys:
+        db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).\
+                update({"stock_name": v['stock_name']})
+        db_info.query(Concise_Table).filter(Concise_Table.fm_id == a_key).update({"unit": v['unit']})
+    db_info.commit()
+
+    for i in d.keys():
+        if i in set_last.keys():
+            buf = set_last.pop(i)
+        if i in set_this.keys():
+            buf = set_this.pop(i)
+        tmp = name.get(i,-99)
+        if tmp!="0" and tmp!=-99:
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == db_keys[0]).update({name[i]: d[i][0]})
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == db_keys[1]).update({name[i]: d[i][1]})
+        elif tmp==-99:
+            print(i)
+
+    db_info.commit()
+
+    # ----------save json --------------
+    path = './' + stock_name + '/' + stock_name + str(v['year'])+'.json'
+    if len(set_this)!=0:
+        a.save_dict_to_json(path,set_this)
+    else:
+        if os.path.exists(path):
+            os.remove(path)
+
+    path = './' + stock_name + '/' + stock_name + str(int(v['year'])-1)+'.json'
+    if len(set_this)!=0:
+        # 保留原有输入数据
+        if os.path.exists(path):
+            tmp = a.load_from_json(path)
+            for i in set_this:
+                if i in tmp:
+                    set_this[i] = tmp[i]
+        a.save_dict_to_json(path,set_last)
+    else:
+        if os.path.exists(path):
+            os.remove(path)
+
+    
+    #  except:
+        #  #write in log
+        #  with open("./log.txt","w") as f:
+            #  f.write("Error when extracting tables from " + key +"\n")
 
 
 
--- read_summary.py
+++ read_summary.py
@@ -4,41 +4,50 @@ from sqlalchemy import exists
 from pprint import pprint
 
 a = FmUtils()
-for ii in range(2010,2018):
-    b = a.load_from_json("./伟星新材/info.json")
-    key = '伟星新材'+str(ii)+'.pdf'
+stock_name = '万华化学'
+
+for ii in range(2011,2018):
+    b = a.load_from_json('./'+stock_name+'/info.json')
+
+    key = stock_name+str(ii)+'.pdf'
     v = b[key]
     c = v['summary_pages']
-    path = './伟星新材/'+key
+    path = './'+stock_name+'/'+key
     pages = str(c[0])+'-'+str(c[1])
     print(path)
-    d = a.read_summary(path,pages)
-    pprint(d)
+    try:
+        d = a.read_summary(path,pages)
+        pprint(d)
 
-    res = []
-    for i in d:
-        if '经常性损益的净' in i:
-            res.append(d[i])
-        if '资产收益率' in i:
-            res.append(d[i])
-    pprint(res) 
+        res = [(0,0),(0,0)]
+        for i in d:
+            if '经常性损益的净' in i or '经常性损益后的净' in i:
+                if res[0] == (0,0):
+                    res[0] = d[i]
+            if '资产收益率' in i:
+                res[1] = d[i]
+        pprint(res) 
 
-    keys = [str(v['stock_id'])+'_'+str(v['year']), str(v['stock_id'])+'_'+str(int(v['year'])-1)]
-    for a_key in keys:
-        (r,) =db_info.query(exists().where(Concise_Table.fm_id==a_key))
-        if not(r[0]):
-            db_info.add(Concise_Table(stock_id = v['stock_id'],date = a_key[-4:]))
-            db_info.commit()
+        keys = [str(v['stock_id'])+'_'+str(v['year']), str(v['stock_id'])+'_'+str(int(v['year'])-1)]
+        for a_key in keys:
+            (r,) =db_info.query(exists().where(Concise_Table.fm_id==a_key))
+            if not(r[0]):
+                db_info.add(Concise_Table(stock_id = v['stock_id'],date = a_key[-4:]))
+                db_info.commit()
+        db_info.commit()
 
+        name = ['smr_deducted_net_profit','smr_avg_roe']
+        if res[0] != (0,0):
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[0]: res[0][0]})
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[0]: res[0][1]})
+        if res[1] != (0,0):
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[1]: res[1][0]})
+            db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[1]: res[1][1]})
 
-    db_info.commit()
-    
-    name = ['smr_deducted_net_profit','smr_avg_roe']
-    db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[0]: res[0][0]})
-    db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[0]: res[0][1]})
-    db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[0]).update({name[1]: res[1][0]})
-    db_info.query(Concise_Table).filter(Concise_Table.fm_id == keys[1]).update({name[1]: res[1][1]})
-
-    db_info.commit()
+        db_info.commit()
+    except:
+        #write in log
+        with open("./log.txt","w") as f:
+            f.write("Error when extracting summary from " + key +"\n")
 
 
